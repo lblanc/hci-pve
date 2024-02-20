b7d5cb8b97545a2c59e5dd945a8f949f6bc32846
Luc BLANC <Email@luc-blanc.com>
2024-02-20 09:28:24 +0100

    add

--- add-vdisk-gfs2.sh
+++ add-vdisk-gfs2.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+nodes="10.12.109.101 10.12.109.102"
+
+echo
+echo "This script will add DataCore vDisk to a PVE Cluster as a GFS2"
+echo
+read -p "Continue? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
